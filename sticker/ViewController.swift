2eebe982e8507e2874c62c30bf84ebb7cde9b9d5
YehYungCheng <yehyungcheng@yungchengtekimacbook-air.local>
2016-03-22 11:44:57 +0800

    Initial Commit

---
+++ sticker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sticker
+//
+//  Created by YehYungCheng on 2016/3/22.
+//  Copyright © 2016年 YehYungCheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
